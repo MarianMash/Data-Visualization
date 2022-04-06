04124e320659920e73c974cfc18743f6fee4a28d
MarianMash <96008364+MarianMash@users.noreply.github.com>
2022-04-06 17:11:14 +0200

    Add files via upload

---
+++ project.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import plotly.express as px  # (version 4.7.0 or higher)
+from utils import gen_sheet
+
+from dash import Dash, dcc, html, Input, Output 
+
+app = Dash(__name__)
+# ---------------------------------------------------------------------------------
+# Data cleaning
+file = pd.ExcelFile("/Users/masha/Desktop/NOVA/Data Visualization/Project/Enerdata_Energy_Statistical_Yearbook_2021.xlsx")
+#file = pd.ExcelFile("Enerdata_Energy_Statistical_Yearbook_2021.xlsx")
+
+file_dict = {key:i for i, key in enumerate(file.sheet_names)}
+
+sheet21 = gen_sheet(file, 'Share of renewables in electri')
+
+
+# ------------------------------------------------------------------------------
+# App layout
+app.layout = html.Div([
+
+    html.H1("First Test for DV", style={'text-align': 'center'}),
+
+    dcc.Slider(id='my_slider',
+                min = 1990, 
+                max = 2020, 
+                step = 1, 
+                value=1990,  
+                marks = None,
+                tooltip={"placement": "bottom", "always_visible": True},
+                updatemode='drag'),
+                
+    html.Div(id='output_container_slider'),
+    
+    html.Br(),
+    ])
+
+# ------------------------------------------------------------------------------
+# Connect Dash-Components to Data
+@app.callback(
+    Output('output_container_slider', 'children'),
+    Input('my_slider', 'value'))
+def update_output(value):
+    return 'You have selected "{}"'.format(value)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ utils.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def gen_sheet(originfile, sheetname):
+    file_dict = {key:i for i, key in enumerate(originfile.sheet_names)}
+    globals()[f"sheet{file_dict[sheetname]}"] = pd.read_excel(originfile,file_dict[sheetname])
+    globals()[f"sheet{file_dict[sheetname]}"].iloc[1,globals()[f"sheet{file_dict[sheetname]}"].columns.get_loc("Unnamed: 0")] = globals()[f"sheet{file_dict[sheetname]}"].columns[1]
+    globals()[f"sheet{file_dict[sheetname]}"].columns = globals()[f"sheet{file_dict[sheetname]}"].iloc[1]
+    globals()[f"sheet{file_dict[sheetname]}"] = globals()[f"sheet{file_dict[sheetname]}"][2:59]
+    return globals()[f"sheet{file_dict[sheetname]}"]
