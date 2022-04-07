af885c3427c2acc35c954dcb1e36c1573f437478
nicolaandreatta <77008481+nicolaandreatta@users.noreply.github.com>
2022-04-07 13:07:53 +0100

    bar chart

--- project.py
+++ project.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 import pandas as pd
 import plotly.express as px  # (version 4.7.0 or higher)
 
@@ -10,7 +11,7 @@ app = Dash(__name__)
 file = pd.ExcelFile("Dataset1.xlsx")
 
 file_dict = {key:i for i, key in enumerate(file.sheet_names)}
-sheet21 = pd.read_excel(file,file_dict['Share of renewables in electri'])
+sheet1 = pd.read_excel(file,file_dict['Total energy production'])
 
 
 # ------------------------------------------------------------------------------
@@ -31,16 +32,47 @@ app.layout = html.Div([
     html.Div(id='output_container_slider'),
     
     html.Br(),
+
+    dcc.Graph(id='my_bar_chart', figure={})
+
     ])
 
 # ------------------------------------------------------------------------------
-# Connect Dash-Components to Data
-@app.callback(
-    Output('output_container_slider', 'children'),
-    Input('my_slider', 'value'))
-def update_output(value):
-    return 'You have selected "{}"'.format(value)
 
+# Connect the Plotly graphs with Dash Components
+@app.callback(
+    [Output(component_id='output_container_slider', component_property='children'),
+     Output(component_id='my_bar_chart', component_property='figure')],
+    [Input(component_id='my_slider', component_property='value')]
+)
+def update_graph(value):
+
+    container = 'You have selected "{}"'.format(value)
+
+    dff = sheet1.copy()
+    dff = dff[["Total energy production (Mtoe)",value]]
+
+    # Plotly Express
+    fig = px.bar(dff, x='Total energy production (Mtoe)', y=value)
+    # Plotly Graph Objects (GO)
+    # fig = go.Figure(
+    #     data=[go.Choropleth(
+    #         locationmode='USA-states',
+    #         locations=dff['state_code'],
+    #         z=dff["Pct of Colonies Impacted"].astype(float),
+    #         colorscale='Reds',
+    #     )]
+    # )
+    #
+    # fig.update_layout(
+    #     title_text="Bees Affected by Mites in the USA",
+    #     title_xanchor="center",
+    #     title_font=dict(size=24),
+    #     title_x=0.5,
+    #     geo=dict(scope='usa'),
+    # )
+
+    return container, fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
