b2fce2523a63e9f4f45553fed1bd73925eaeb5b1
hampet97 <hampet97@gmail.com>
2022-04-07 13:50:29 +0100

    electricity grpah

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import plotly.express as px  # (version 4.7.0 or higher)
+import plotly.graph_objects as go
 
 
 from dash import Dash, dcc, html, Input, Output 
@@ -12,6 +13,22 @@ file = pd.ExcelFile("Dataset1.xlsx")
 file_dict = {key:i for i, key in enumerate(file.sheet_names)}
 sheet21 = pd.read_excel(file,file_dict['Share of renewables in electri'])
 
+
+
+#-------------------------------------------------------------------------------------
+# Building the graphs 
+
+# GRAPH 1 - ELECTRICITY
+dfElectricity = pd.read_csv('Data-Visualization/electricity.csv')
+country_list = ['Portugal', 'Romania', 'Spain', 'Sweden', 'United Kingdom']
+
+options1 = [{'label': 'Portugal', 'value': 'Portugal'},
+           {'label': 'Romania', 'value': 'Romania'},
+           {'label': 'Spain', 'value': 'Spain'}, 
+           {'label': 'Sweden', 'value': 'Sweden'}, 
+           {'label': 'United Kingdom', 'value': 'United Kingdom'}]
+
+# GRAPH 2 - 
 
 # ------------------------------------------------------------------------------
 # App layout
@@ -31,6 +48,22 @@ app.layout = html.Div([
     html.Div(id='output_container_slider'),
     
     html.Br(),
+
+    html.H3("Electricity shit", style={'text-align': 'center'}),
+    html.Br(),
+    html.Label('Choose a Country:'),
+    dcc.Dropdown(
+        id='drop1',
+        options=options1,
+        value='Portugal'
+    ),
+
+    dcc.Graph(
+        id='electricity_graph'
+    ),
+
+    html.Br()
+
     ])
 
 # ------------------------------------------------------------------------------
@@ -40,6 +73,27 @@ app.layout = html.Div([
     Input('my_slider', 'value'))
 def update_output(value):
     return 'You have selected "{}"'.format(value)
+
+
+
+@app.callback(
+    Output(component_id='electricity_graph', component_property='figure'),
+    [Input(component_id='drop1', component_property='value')]
+)
+def callback_1(input_value):
+    data1 = [dict(type='scatter',
+                x=dfElectricity.index,
+                y=dfElectricity[country],
+                name=country)
+                                for country in country_list]
+    layout1 = dict(title=dict(
+                            text='Electricity emissions of 5 countries between 1990-2020'
+                    ),
+                    xaxis=dict(title='Years'),
+                    yaxis=dict(title='Electricity Emissions'))
+    return go.Figure(data=data1, layout=layout1)
+
+
 
 
 if __name__ == '__main__':
