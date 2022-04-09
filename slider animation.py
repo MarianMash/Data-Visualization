04d27d4db1d90e8f45fe0678ea73f84e1c2920f5
MarianMash <96008364+MarianMash@users.noreply.github.com>
2022-04-09 22:34:25 +0200

    tab1

--- slider animation.py
+++ slider animation.py
@@ -47,8 +47,12 @@ app.layout = html.Div([
         html.Br(),
         html.Br(),
         html.Div(id='msg-container'),
-    ], className = 'row'),
     
+    ], className = 'row'),
+    html.Div(children=[
+                dcc.Graph(id="bar_hor_1", style={'display': 'inline-block','width': '34%'}),
+                dcc.Graph(id="world", style={'display': 'inline-block','width': '64%'}),
+                ]),
 ])
 
 
@@ -101,6 +105,93 @@ def on_click(n_intervals, buttonReset, dragValue):
         n_intervals = int(dragValue) -1990
 
     return 1990 + (n_intervals)%30, 0, n_intervals
+
+@app.callback(
+    [Output(component_id='world',component_property = 'figure'),
+    Output(component_id='bar_hor_1',component_property = 'figure'),
+    Output(component_id = 'sort_button', component_property = 'children')],
+
+    [Input(component_id = 'steper', component_property = 'value'),
+    Input(component_id = 'sort_button', component_property = 'n_clicks')])
+
+
+def Mapping(selected_year,sort_button_value):
+    dff = df.copy()
+    dff = dff[dff["Year"]==selected_year]
+
+    # Create figure
+    fig = px.choropleth_mapbox(
+        dff,
+        geojson=gj,
+        featureidkey = "properties.iso_a3",
+        locations="iso_a3",
+        color=dff['Total energy production (Mtoe)'],
+        color_continuous_scale='Darkmint',
+        range_color=(0, dff['Total energy production (Mtoe)'].max()),
+
+        animation_frame = dff.Year,
+        animation_group = dff.iso_a3,
+
+        hover_name='Country', # here maybe Country
+        hover_data={'Country': True, 'Total energy production (Mtoe)': True,"iso_a3":False},
+        mapbox_style='light',
+        zoom=1.01,
+        center={'lat': 19, 'lon': 11},
+        opacity=0.6
+    )
+
+    # Define layout specificities
+    fig.update_layout(dragmode=False,
+    margin={'r':0,'t':0,'l':0,'b':0},
+        coloraxis_colorbar={
+            'title':'Mtoe',
+            'tickvals':(0,round(dff['Total energy production (Mtoe)'].max())),
+         }
+         )
+    fig.update_coloraxes(colorbar_xanchor="left",   
+                            #colorbar_x= -0.01,
+                            colorbar_title_side = "right",
+                            colorbar_orientation = "v",
+                            colorbar_ticks = "inside")
+
+
+
+
+
+    #horizontal barplot
+    if sort_button_value%2:
+        dfff=dff.nsmallest(10, ['Total energy production (Mtoe)']).sort_values('Total energy production (Mtoe)', ascending=False)
+        
+        button_text = "Smallest"
+    else: 
+        dfff=dff.nlargest(10, ['Total energy production (Mtoe)']).sort_values('Total energy production (Mtoe)', ascending=True)
+        button_text = "Largest"
+
+    bar_hor = px.bar(dfff, 
+                x='Total energy production (Mtoe)', 
+                y="Country", orientation='h',
+                barmode = "group",
+                color=dfff['Total energy production (Mtoe)'],
+                color_continuous_scale='Darkmint',
+                range_color=(round(dfff.iloc[9]['Total energy production (Mtoe)']), round(dfff.iloc[0]['Total energy production (Mtoe)'])),
+    )
+    # bar_hor.update_layout(coloraxis_colorbar={
+    #         'title':'Mtoe',
+    #         'tickvals':(0,round(dff.iloc[0]['Total energy production (Mtoe)'])),
+    #         #'ticktext':ticks        
+    #      })
+    bar_hor.update_layout({
+            "plot_bgcolor": "rgba(0, 0, 0, 0)",
+            "paper_bgcolor": "rgba(0, 0, 0, 0)",
+            },margin={'r':0,'t':0,'l':15,'b':0})
+    bar_hor.update_traces(text=list(dfff.Country), textposition='inside',textfont_color='White')
+    bar_hor.update_yaxes(visible=False, showticklabels=False)
+    bar_hor.update_xaxes(visible=False, showticklabels=False)
+    bar_hor.update(layout_coloraxis_showscale=False)
+
+
+
+    return fig, bar_hor, button_text 
 
 
 if __name__ == '__main__':
