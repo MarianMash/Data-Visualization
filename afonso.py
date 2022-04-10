7058f0be8ca4636b1ca1eb1ad50b9fb742ef61b8
afonsocmg1 <90836695+afonsocmg1@users.noreply.github.com>
2022-04-10 22:48:48 +0100

    afonso

--- afonso.py
+++ afonso.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import plotly.express as px  # (version 4.7.0 or higher)
 import plotly.graph_objects as go
-
-
+import geojson
 from dash import Dash, dcc, html, Input, Output, callback
 
 app = Dash(__name__)
 # ---------------------------------------------------------------------------------
 # Data import and cleaning
 file = pd.read_csv("ActualDataset.csv")
+
+with open("geojson11.geojson") as f:
+    gj = geojson.load(f)
 
 countries = []
 for country in file['Country'].unique():
@@ -43,7 +45,7 @@ app.layout = html.Div([
     html.Br(),
 
     dcc.Graph(id='bar_chart_2'),
-
+    
     html.Br(),
 
     dcc.Slider(id='slider',
@@ -66,7 +68,14 @@ app.layout = html.Div([
 
     html.Br(),
 
-    dcc.Graph(id='circle_graph')
+    dcc.Graph(id='circle_graph'),
+    html.Br(),
+
+    dcc.Graph(id='circle_graph_2'),
+    html.Br(),
+
+    dcc.Graph(id='geo_bubble_graph')
+    
     ])
 
 # ------------------------------------------------------------------------------
@@ -75,6 +84,7 @@ app.layout = html.Div([
 @callback(
             [Output(component_id='bar_chart', component_property='figure'),
             Output(component_id='bar_chart_2', component_property='figure')],
+
             [Input(component_id='year_slider', component_property='value')])
 
 
@@ -179,11 +189,14 @@ def update_bar_graph(value):
 
 #circle graph of total consumption of all features by country
 @callback(
-            Output(component_id='circle_graph', component_property='figure'),
+            [Output(component_id='circle_graph', component_property='figure'),
+            Output(component_id='circle_graph_2', component_property='figure')],
             [Input(component_id='country_dropdown', component_property='value'),
             Input(component_id='slider', component_property='value')])
 
 def update_circle_graph(country_dropdown, value):
+
+    #circle graph of total consumption of all features by country
     labels_dict ={'Oil products domestic consumption (Mt)': 'Oil',
            'Natural gas domestic consumption (bcm)': 'Gas',
            'Coal and lignite domestic consumption (Mt)': 'Coal', 
@@ -202,8 +215,70 @@ def update_circle_graph(country_dropdown, value):
                 names= list(labels_dict.values()),
                 hole=.8)
 
-    return piechart
 
+    #circle graph percentage of renewables and non renewablesin eletricity production
+
+    labels_dict_2 ={'Share of renewables in electricity production (%)': 'Renewable Energy',
+           'Non Renewable Energy': 'Non Renewable Energy'}
+
+    dff3 = file.copy()
+    dff3 = dff3[dff3['Year'] == value]
+    dff3 = dff3[dff3['Country'] == country_dropdown]
+    dff3 = dff3[['Share of renewables in electricity production (%)']]
+    dff3['Share of non renewables in electricity production (%)'] = 100 - dff3['Share of renewables in electricity production (%)']
+
+    piechart_2 =px.pie(
+                data_frame=dff3,
+                values = dff3.values.tolist()[0],
+                names= list(labels_dict_2.values()),
+                hole=.8)
+ 
+    return piechart, piechart_2
+
+##geo bubble map of renewables######################
+@callback(
+            Output(component_id='geo_bubble_graph', component_property='figure'),
+            [Input(component_id='slider', component_property='value')])
+
+def geo_bubble_graph(value):
+
+    dff4 = file.copy()
+    dff4 = dff4[dff4['Year'] == value]
+    dff4 = dff4[['Share of renewables in electricity production (%)','iso_a3','Country']]
+    dff4 = dff4.dropna()
+    dff4['text'] = dff4['Country'] + '<br>Share of renewables in electricity production: ' + (dff4['Share of renewables in electricity production (%)']).astype(str)+' %'
+    limits = [(0,20),(20,40),(41,60),(61,80),(81,100)]
+    colors = ["royalblue","crimson","lightseagreen","orange","chocolate"]
+
+    geomap = go.Figure()
+
+    for i in range(len(limits)):
+        lim = limits[i]
+        df_sub = dff4[(dff4['Share of renewables in electricity production (%)'] > lim[0]) & 
+                (dff4['Share of renewables in electricity production (%)'] < lim[1])]
+        geomap.add_trace(go.Scattergeo(
+            geojson=gj,
+            featureidkey = "properties.iso_a3",
+            locations= df_sub["iso_a3"],
+            text = df_sub['text'],
+            marker = dict(
+                size = df_sub['Share of renewables in electricity production (%)']**(i+1),
+                color = colors[i],
+                line_color='rgb(40,40,40)',
+                line_width =0.5,
+                sizemode = 'area'
+            ),
+            name = '{0} - {1}'.format(lim[0],lim[1])))
+
+    geomap.update_layout(
+            title_text = 'Share of renewables in electricity production World Bubble Map',
+            showlegend = True,
+            geo = dict(
+                scope = 'world',
+                landcolor = 'rgb(217, 217, 217)',
+            )
+        )
+    return geomap
 #----------------------------------------
 if __name__ == '__main__':
     app.run_server(debug=True)
